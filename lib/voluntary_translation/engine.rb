57be0778584a0f3634db8f7039f2043ff7c01df6
Mathias Gawlista <gawlista.mathias@vimn.com>
2014-03-20 14:23:55 +0100

    Adds translation product to core navigation.

--- lib/voluntary_translation/engine.rb
+++ lib/voluntary_translation/engine.rb
@@ -1,5 +1,9 @@
 module VoluntaryTranslation
   class Engine < ::Rails::Engine
     config.i18n.load_path += Dir[File.expand_path("../../../config/locales/**/*.{rb,yml}", __FILE__)]
+    
+    config.to_prepare do
+      Voluntary::Navigation::Base.add_product('translation', t('workflow.user.products.translation.title'))
+    end
   end
 end
