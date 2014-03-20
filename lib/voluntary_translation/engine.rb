439be59f2e8aa5a739404b5695555c01eb43743a
Mathias Gawlista <gawlista.mathias@vimn.com>
2014-03-20 14:48:47 +0100

    Use I18n.t instead of t.

--- lib/voluntary_translation/engine.rb
+++ lib/voluntary_translation/engine.rb
@@ -3,7 +3,7 @@ module VoluntaryTranslation
     config.i18n.load_path += Dir[File.expand_path("../../../config/locales/**/*.{rb,yml}", __FILE__)]
     
     config.to_prepare do
-      Voluntary::Navigation::Base.add_product('translation', t('workflow.user.products.translation.title'))
+      Voluntary::Navigation::Base.add_product('translation', I18n.t('workflow.user.products.translation.title'))
     end
   end
 end
